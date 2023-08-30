7505f71e7677a19d25305bf407ae6fbf6db81766
MachunuruSreenivasulaRedd <918296629y@gmail.com>
2023-08-30 06:34:26 +0000

    saved

---
+++ src/components/About/index.js
@@ -0,0 +1,26 @@
+import './index.css'
+
+const About = () => (
+  <div className="ContetentStruct">
+    <div className="BannerContentHead">SERVING HAPPINESS!</div>
+    <div className="BannerContent">
+      <p>
+        As a flagship venture of the USD 5 Bn SHOW TIME Group, our Leisure
+        Limited has always worked upon enhancing the experiences, right from
+        creating a world-class infrastructure which is high on comfort and
+        aesthetics, to staying updated with the latest in the cinema technology
+        space. Ever since commencing operations in year 2002, SHOW TIME has
+        already entertained more than 700 Mn guests in its glorious journey.
+        With
+        <span id="ctl00_ContentPlaceHolder1_LblScreens2">746</span>
+        perfectly appointed screens in
+        <span id="ctl00_ContentPlaceHolder1_LblTheatreCount">173</span>
+        multiplexes across the country, SHOW TIME continues to get closer to the
+        Indian cinema lovers, and also remain on a growth path which is envied
+        across the globe.
+      </p>
+    </div>
+  </div>
+)
+
+export default About
